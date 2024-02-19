be872c652444f83d8b6f711149fa9f6b159ffda7
riggs <jonrigert@gmail.com>
2024-02-19 10:27:35 -0500

    simplifying image

--- src/components/core/ImageCircle.tsx
+++ src/components/core/ImageCircle.tsx
@@ -4,31 +4,35 @@ import { useHasLoaded } from '@/hooks/useHasLoaded';
 import { classNames } from '@/utils/style';
 import Image, { ImageProps } from 'next/image';
 import { FunctionComponent } from 'react';
-import styles from './ImageCircle.module.css';
 
 export interface ImageCircleProps
   extends Omit<ImageProps, 'fill' | 'className'> {}
 
 export const ImageCircle: FunctionComponent<ImageCircleProps> = (props) => {
+  const { alt, ...imageProps } = props;
   const { hasLoaded, onLoad } = useHasLoaded();
-  const animationClass = hasLoaded ? styles['animate'] : undefined;
-  const containerClass = hasLoaded ? 'opacity-100' : 'opacity-0';
+  const containerAnimationClass = hasLoaded ? 'opacity-100' : 'opacity-0';
+  const imageAnimationClass = hasLoaded
+    ? 'opacity-100 scale-100'
+    : 'opacity-0 scale-90';
 
   return (
     <div
       className={classNames(
-        'relative h-52 w-52 overflow-clip opacity-0',
-        containerClass,
+        'relative h-56 w-56 overflow-hidden rounded-full outline outline-4 outline-offset-8 outline-foreground/70 transition duration-[2s] ease-out',
+        containerAnimationClass,
       )}
     >
-      <div className="absolute h-full w-full p-4">
-        <div className="h-full w-full rounded-xl bg-primary opacity-15" />
-      </div>
+      <div className="absolute h-full w-full rounded-xl bg-primary opacity-60" />
 
       <Image
-        className={classNames(styles['image-circle'], animationClass)}
+        className={classNames(
+          'mt-3 transition duration-500',
+          imageAnimationClass,
+        )}
         onLoad={onLoad}
-        {...props}
+        alt={alt}
+        {...imageProps}
         fill={true}
       />
     </div>
